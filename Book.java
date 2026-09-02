6807893ae0976b73e03039b1605c1f2362364a2a
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-02 21:34:02 +0300

    using compact constructor

--- Book.java
+++ Book.java
@@ -1,5 +1,14 @@
 public record Book(String title, String author, int price) {
-    
+
+    public Book{
+        if (price < 0){
+            throw new IllegalArgumentException("Price can't be negative.");
+        }
+        if (title.equals("")){
+            throw new IllegalArgumentException("Title can't be blank.");
+        }
+    }
+
     public static void main(String[] args) {
         Book book = new Book("I want you to be happy", "Jem Calder", 26);
         System.out.println(book.title());

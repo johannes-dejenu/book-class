51c94d8cee14ea5a8958cdfadd22b2fa3b43c422
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-02 20:28:00 +0300

    learning about the record topic which is important for using java class as a boilerplate

--- Book.java
+++ Book.java
@@ -1,35 +1,9 @@
-public class Book {
-    private String title;
-    private String author;
-    private int price;
-
-
-    public Book setTitle(String title){
-        this.title = title;
-        return this;
-    }
-
-    public Book setAuthor(String author){
-        this.author = author;
-        return this;
-    }
-
-    public Book setPrice(int price){
-        this.price = price;
-        return this;
-    }
-    public void displayInfo(){
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(price);
-    }
-
+public record Book(String title, String author, int price) {
+    
     public static void main(String[] args) {
-        Book book = new Book();
-        book.setTitle("I want you to be happy")
-            .setAuthor("Jem Calder")
-            .setPrice(26);
-        book.displayInfo();
+        Book book = new Book("I want you to be happy", "Jem Calder", 26);
+        System.out.println(book.title());
+        System.out.println(book.author());
+        System.out.println(book.price());
     }
-
 }
